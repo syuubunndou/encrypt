5348e91a38cd6846708645cc8107e5e395e6cbf6
syuubunndou <zin.syuubunndou@gmail.com>
2025-03-14 12:50:26 +0900

    Update index.py

--- index.py
+++ index.py
@@ -6,36 +6,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/encrypt": {"origins": "*"}})  # 必要に応じてオリジンを指定
 
+@app.route('/get_message', methods=['GET'])
+def get_message():
+    return jsonify({"message": "foo"})
 
-# 暗号化キーを生成 (実運用なら .env などで管理)
-key = Fernet.generate_key()
-cipher = Fernet(key)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
-@app.route('/encrypt', methods=['GET'])
-def encrypt_data():
-    print("これ見えてる？")
-    print("or is it?")
-    try:
-        data = request.get_json()
-        uid = data.get("UID");
-        createAt = data.get("FIREBASE_CREATE_AT"); 
-
-        
-        if not uid or not createAt:
-            return jsonify({"error": "Invalid data"}), 400
-        
-        # ハッシュ生成
-        combined = f"{uid}{createAt}";
-        hashed = hashlib.sha256(combined.encode()).hexdigest()
-        
-        # 暗号化
-        encrypted_data = cipher.encrypt(hashed.encode()).decode()
-        
-        return jsonify({"encrypted_data": encrypted_data})
-    
-    except Exception as e:
-        print(f"エラー発生: {e}")  # エラーメッセージを表示
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
